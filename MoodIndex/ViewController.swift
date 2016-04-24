34db774ad1e2d82e4813d33faf490e5aa7ee590f
Malik Graham <malikg@fondowt-1.student.lawrence.edu>
2016-04-23 19:45:50 -0500

    Fixed the layout correctly

--- MoodIndex/ViewController.swift
+++ MoodIndex/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var moodTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        /*let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Day], fromDate: date)
+        var num = 24;
+        let date_1 = date.dateByAddingTimeInterval(60*60*24)
+        print(calendar.component([.Day], fromDate: date_1))*/
+        
+        /*for num in 0...6 {
+            moodTable.dequeueReusableCellWithIdentifier(<#T##identifier: String##String#>)
+        }*/
+ }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
