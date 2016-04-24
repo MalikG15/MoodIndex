eb34e0d40f509696b1b3dd0ca2b52779e73b492d
MalikG15 <malikgraham15@yahoo.com>
2016-04-23 20:14:40 -0500

    Add more TO DO

--- MoodIndex/ViewController.swift
+++ MoodIndex/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Iterate over the array and get each day
+        // and then add it to the table
         // Do any additional setup after loading the view, typically from a nib.
         /*let date = NSDate()
         let calendar = NSCalendar.currentCalendar()
