f1f1634e6e2dc468ae33ef1eff30521b9a4124d2
MalikG15 <malikgraham15@yahoo.com>
2016-04-23 21:49:01 -0500

    Added functionality that shows dates in tableView

--- MoodIndex/ViewController.swift
+++ MoodIndex/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate {
     
-    //@IBOutlet weak var moodTable: UITableView!
+    @IBOutlet weak var moodTable: UITableView!
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 7;
@@ -19,35 +19,24 @@ class ViewController: UIViewController, UITableViewDelegate {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
         
-        /*let date = NSDate()
+        let date = NSDate()
         let calendar = NSCalendar.currentCalendar()
         var numDay:Double = 24.0
         for _ in 0...6 {
+            var tableDate = ""
             let moveDate = date.dateByAddingTimeInterval(60*60*numDay)
-            cell.textLabel?.text = String(calendar.component([.Day], fromDate: moveDate))
+            tableDate += String(calendar.component([.Month], fromDate: moveDate)) + " - "
+            tableDate += String(calendar.component([.Day], fromDate: moveDate)) + " - "
+            tableDate += String(calendar.component([.Year], fromDate: moveDate))
+            cell.textLabel?.text = tableDate
             numDay = numDay + 24
-        }*/
-        
-        cell.textLabel?.text = "Hey Bud"
+        }
         
         return cell
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Iterate over the array and get each day
-        // and then add it to the table
-        // Do any additional setup after loading the view, typically from a nib.
-        /*let date = NSDate()
-        let calendar = NSCalendar.currentCalendar()
-        let components = calendar.components([.Day], fromDate: date)
-        var num = 24;
-        let date_1 = date.dateByAddingTimeInterval(60*60*24)
-        print(calendar.component([.Day], fromDate: date_1))*/
-        
-        /*for num in 0...6 {
-            moodTable.dequeueReusableCellWithIdentifier(<#T##identifier: String##String#>)
-        }*/
  }
 
     override func didReceiveMemoryWarning() {
