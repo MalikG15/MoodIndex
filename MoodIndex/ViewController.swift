ee02c23c7d32fa16610544d413a3567f86277be1
MalikG15 <malikgraham15@yahoo.com>
2016-04-23 22:05:06 -0500

    Modified code so that each individual date for next 7 days appears

--- MoodIndex/ViewController.swift
+++ MoodIndex/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController, UITableViewDelegate {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
         
+        var next7DaysDates = [String]()
+        
         let date = NSDate()
         let calendar = NSCalendar.currentCalendar()
         var numDay:Double = 24.0
@@ -28,9 +30,13 @@ class ViewController: UIViewController, UITableViewDelegate {
             tableDate += String(calendar.component([.Month], fromDate: moveDate)) + " - "
             tableDate += String(calendar.component([.Day], fromDate: moveDate)) + " - "
             tableDate += String(calendar.component([.Year], fromDate: moveDate))
-            cell.textLabel?.text = tableDate
+            next7DaysDates.append(tableDate)
             numDay = numDay + 24
         }
+        
+        cell.textLabel?.text = next7DaysDates[indexPath.row]
+        
+        //cell.imageView?.setValue(<#T##value: AnyObject?##AnyObject?#>, forKeyPath: <#T##String#>)
         
         return cell
     }
